81f2201bda6ef2ff9412e3af8377c0a770d25498
Sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2020-04-26 16:52:51 +0200

    correction string

--- graphql/schema/patient.js
+++ graphql/schema/patient.js
@@ -13,7 +13,7 @@ module.exports = {
             homeLat: Float!
             homeLon: Float!
             imageUrl: String!
-            risk_factor: String!
+            risk_factor: String
         }
     `,
     resolvers: {
